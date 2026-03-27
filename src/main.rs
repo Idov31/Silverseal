3316d64bf0fa7a6407fa6fa9bbf3cef8a64d820e
idov31 <62358580+Idov31@users.noreply.github.com>
2026-03-27 18:34:12 +0300

    Implemented hooking logic

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,13 @@ use core::time::Duration;
 use log::info;
 use uefi::boot::{self};
 use uefi::prelude::*;
+use uefi::proto::loaded_image::LoadedImage;
 
 pub mod helpers;
-use crate::helpers::file_helper::{
-    load_original_grub
+use crate::helpers::{
+    file_helper::load_original_grub,
+    hooks::{GRUB_ARCH_EFI_LINUX_BOOT_IMAGE_HOOK_INLINE, grub_arch_efi_linux_boot_image_hook},
+    memory_helper::{LINUX_BOOT_IMAGE_SIGNATURE, binary_search, inline_hook},
 };
 
 #[entry]
@@ -22,11 +25,44 @@ fn main() -> Status {
         Ok(handle) => handle,
         Err(e) => {
             info!("Failed to load original GRUB: {:?}", e);
+            boot::stall(Duration::from_secs(5));
             return e.status();
         }
     };
-    info!("Original GRUB handle: {:?}", original_grub_handle);
-    boot::stall(Duration::from_secs(5));
+    let loaded_image = match boot::open_protocol_exclusive::<LoadedImage>(original_grub_handle) {
+        Ok(image) => image,
+        Err(e) => {
+            info!("Failed to open LoadedImage protocol: {:?}", e);
+            boot::stall(Duration::from_secs(5));
+            return e.status();
+        }
+    };
+    let (base, size) = loaded_image.info();
+    info!("Original GRUB base: {:?}, size: {:?}", base, size);
+    let target = match binary_search(base as usize, size as usize, LINUX_BOOT_IMAGE_SIGNATURE) {
+        Some(addr) => addr,
+        None => {
+            info!("Failed to find target function in original GRUB");
+            boot::stall(Duration::from_secs(5));
+            return Status::NOT_FOUND;
+        }
+    };
+    info!("Found target function at address: {:#x}", target);
+
+    // Install the hook at the found address
+    let linux_boot_image_hook = grub_arch_efi_linux_boot_image_hook as *const() as usize;
+
+    let hook_info = match inline_hook(target, linux_boot_image_hook) {
+        Ok(info) => info,
+        Err(e) => {
+            info!("Failed to install inline hook: {:?}", e);
+            boot::stall(Duration::from_secs(5));
+            return e;
+        }
+    };
+    unsafe {
+        GRUB_ARCH_EFI_LINUX_BOOT_IMAGE_HOOK_INLINE = hook_info;
+    }
 
     if let Err(e) = boot::start_image(original_grub_handle) {
         info!("Failed to start original GRUB: {:?}", e);
